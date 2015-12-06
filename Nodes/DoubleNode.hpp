f1e5484f9f3b090a39414f29b8474220d4cb5357
Dravid Joseph <dravidjoseph@gmail.com>
2015-12-05 20:46:55 -0600

    Changed Container to Node- container has another meaning in CS

---
+++ Nodes/DoubleNode.hpp
@@ -0,0 +1,43 @@
+/*******************************************************
+* @file: Node.hpp
+* @author: Dravid Joseph
+* @date: 9/21/15
+* @brief: Implementation file for Node class
+********************************************************/
+
+
+template<typename T>
+Node<T>::Node():m_value(T()),m_previous(nullptr),m_next(nullptr){
+	
+}
+
+template<typename T>
+T Node<T>::getValue() const{
+	return m_value;
+}
+
+template<typename T>
+void Node<T>::setValue(T val){
+	m_value = val;
+}
+
+template<typename T>
+Node<T>* Node<T>::getPrevious() const{
+	return m_previous;
+	
+}
+
+template<typename T>
+void Node<T>::setPrevious(Node<T>* prev){
+	m_previous = prev;
+}
+
+template<typename T>
+Node<T>* Node<T>::getNext() const{
+	return m_next;
+}
+
+template<typename T>
+void Node<T>::setNext(Node<T>* next){
+	m_next = next;
+}
